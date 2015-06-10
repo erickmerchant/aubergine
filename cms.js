15e33a9aa10c1d1504dfa789f85b63babd891854
erickmerchant <erickmerchant@gmail.com>
2015-06-10 02:01:30 -0700

    Change the way we minify css. Change the way we use browserify

--- cms.js
+++ cms.js
@@ -6,7 +6,8 @@ const sergeant = require('sergeant')
 const chalk = require('chalk')
 const bach = require('./bach-extended.js')
 const vinylFS = require('vinyl-fs')
-const defaultSeries = bach.series(bach.parallel(bach.series(pages, icons, minifyHTML), css, js), shortenSelectors, insertCSS, insertJS)
+const fs = require('fs')
+const defaultSeries = bach.series(pages, icons, minifyHTML, css, js)
 const app = sergeant('CMS for chrono')
 
 app.command('update', { description: 'Build the site once' }, defaultSeries)
@@ -43,49 +44,136 @@ function pages () {
   ])
 }
 
-function css () {
-  const cssnext = require('gulp-cssnext')
-  const csso = require('gulp-csso')
+function css (done) {
+  const cssnext = require('cssnext')
+  const cheerio = require('gulp-cheerio')
+  const postcss = require('postcss')
+  const Smallector = require('smallector')
+  const byebye = require('css-byebye')
+  const nano = require('cssnano')
+  const pseudosRegex = /\:?(\:[a-z-]+)/g
 
-  return vinylFS.src('css/app.css')
-    .pipe(cssnext({
-      features: {
-        customProperties: {
-          strict: false
-        },
-        rem: false,
-        pseudoElements: false,
-        colorRgba: false
+  fs.readFile('css/app.css', 'utf-8', function (err, css) {
+    if (err) {
+      done(err)
+    }
+
+    css = cssnext(
+      css, {
+        from: 'css/app.css',
+        features: {
+          customProperties: {
+            strict: false
+          },
+          rem: false,
+          pseudoElements: false,
+          colorRgba: false
+        }
       }
-    }))
-    .pipe(csso())
-    .pipe(vinylFS.dest(directory))
+    )
+
+    vinylFS.src('./index.html')
+      .pipe(cheerio(function ($) {
+        const parsed = postcss.parse(css)
+        var unused = []
+        var ignore = ['.flash']
+        var output
+
+        function trav (nodes) {
+          nodes.forEach(function (node) {
+            if (node.type !== 'atrule' || !node.name.endsWith('frames')) {
+              if (node.selector) {
+                node.selector
+                  .split(',')
+                  .map(function (selector) {
+                    return selector.trim()
+                  })
+                  .forEach(function (selector) {
+                    var _selector = selector.replace(pseudosRegex, function (selector, pseudo) {
+                      return pseudo === ':not' ? selector : ''
+                    })
+
+                    try {
+                      if (_selector && ignore.indexOf(_selector) < 0 && !$(_selector).length) {
+                        unused.push(selector)
+                      }
+                    } catch (e) {
+                      console.error(_selector)
+                    }
+                  })
+              }
+
+              if (node.nodes) {
+                trav(node.nodes)
+              }
+            }
+          })
+        }
+
+        trav(parsed.nodes)
+
+        output = new Smallector(postcss(byebye({ rulesToRemove: unused })).process(css).css, { compress: true })
+
+        $('[class]').each(function () {
+          var classes = $(this).attr('class').split(' ')
+
+          classes.forEach(function (v, k) {
+            if (output.map[v]) {
+              classes[k] = output.map[v]
+            } else {
+              delete classes[k]
+            }
+          })
+
+          $(this).attr('class', classes.join(' ') || null)
+        })
+
+        ignore.forEach(function (v) {
+          if (output.map[v.substr(1)]) {
+            console.log('/\\.' + output.map[v.substr(1)] + '/g')
+
+            output.compiled = output.compiled.replace(new RegExp('\\.' + output.map[v.substr(1)], 'g'), v)
+          }
+        })
+
+        output = postcss(nano()).process(output.compiled).css
+
+        $('head').append(`<style type="text/css">${ output }</style>`)
+      }))
+      .pipe(vinylFS.dest(directory))
+      .on('end', function () {
+        done()
+      })
+  })
 }
 
 function js (done) {
-  const uglify = require('gulp-uglify')
-  const tap = require('gulp-tap')
-  const vinylFS = require('vinyl-fs')
   const browserify = require('browserify')
-  const source = require('vinyl-source-stream')
-  const buffer = require('vinyl-buffer')
+  const uglify = require('uglify-js')
   const collapse = require('bundle-collapser/plugin')
-  const bundler = browserify({
-    entries: './js/app.js',
-    debug: false
-  })
+  const cheerio = require('gulp-cheerio')
+  const bundler = browserify({debug: false})
 
-  bundler
-    .plugin(collapse)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(tap(function (file) {
-      file.contents = new Buffer('!function(window, document, Math, Date){ ' + file.contents + ' }(window, document, Math, Date)')
-    }))
-    .pipe(uglify())
-    .pipe(vinylFS.dest(directory))
-    .on('end', done)
+  bundler.add('js/app.js')
+
+  bundler.plugin(collapse)
+
+  bundler.bundle(function (err, js) {
+    if (err) {
+      done(err)
+    } else {
+      js = js.toString()
+
+      js = uglify.minify(js, {fromString: true, mangle: true}).code
+
+      vinylFS.src('./index.html')
+        .pipe(cheerio(function ($) {
+          $('body').append('<script>' + js + '</script>')
+        }))
+        .pipe(vinylFS.dest(directory))
+        .on('end', done)
+    }
+  })
 }
 
 function minifyHTML () {
@@ -129,101 +217,6 @@ function icons (done) {
     }))
     .pipe(vinylFS.dest(directory))
     .on('end', done)
-}
-
-function shortenSelectors () {
-  const vinylFS = require('vinyl-fs')
-  const selectors = require('gulp-selectors')
-
-  return vinylFS.src(['./index.html', './app.css'])
-    .pipe(selectors.run(undefined, {ids: '*', classes: ['flash']}))
-    .pipe(vinylFS.dest(directory))
-}
-
-function insertCSS (done) {
-  const cheerio = require('gulp-cheerio')
-  const fs = require('fs')
-  const postcss = require('postcss')
-  const byebye = require('css-byebye')
-  const nano = require('cssnano')
-  const pseudosRegex = /\:?(\:[a-z-]+)/g
-  const del = require('del')
-
-  fs.readFile('./app.css', 'utf-8', function (err, css) {
-    if (err) {
-      done(err)
-    }
-
-    vinylFS.src('./index.html')
-      .pipe(cheerio(function ($) {
-        const parsed = postcss.parse(css)
-        var unused = []
-        var ignore = ['.flash']
-        var output
-
-        function trav (nodes) {
-          nodes.forEach(function (node) {
-            if (node.type !== 'atrule' || !node.name.endsWith('frames')) {
-              if (node.selector) {
-                node.selector
-                  .split(',')
-                  .map(function (selector) {
-                    return selector.trim()
-                  })
-                  .forEach(function (selector) {
-                    var _selector = selector.replace(pseudosRegex, function (selector, pseudo) {
-                      return pseudo === ':not' ? selector : ''
-                    })
-
-                    try {
-                      if (_selector && ignore.indexOf(_selector) < 0 && !$(_selector).length) {
-                        unused.push(selector)
-                      }
-                    } catch (e) {
-                      console.error(_selector)
-                    }
-                  })
-              }
-
-              if (node.nodes) {
-                trav(node.nodes)
-              }
-            }
-          })
-        }
-
-        trav(parsed.nodes)
-
-        output = postcss(byebye({ rulesToRemove: unused }), nano()).process(css).css
-
-        $('head').append(`<style type="text/css">${ output }</style>`)
-      }))
-      .pipe(vinylFS.dest(directory))
-      .on('end', function () {
-        del(['./app.css'], done)
-      })
-  })
-}
-
-function insertJS (done) {
-  const cheerio = require('gulp-cheerio')
-  const fs = require('fs')
-  const del = require('del')
-
-  fs.readFile('./bundle.js', 'utf-8', function (err, js) {
-    if (err) {
-      done(err)
-    }
-
-    vinylFS.src('./index.html')
-      .pipe(cheerio(function ($) {
-        $('body').append('<script>' + js + '</script>')
-      }))
-      .pipe(vinylFS.dest(directory))
-      .on('end', function () {
-        del(['./bundle.js'], done)
-      })
-  })
 }
 
 function serve (done) {
