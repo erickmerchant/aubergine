d249a4b07ee865a11a663a5eb8844f9db6ed626a
erickmerchant <erickmerchant@gmail.com>
2015-07-09 22:42:15 -0700

    Find port

--- src/tasks/serve.js
+++ src/tasks/serve.js
@@ -3,22 +3,28 @@
 const express = require('express')
 const _static = require('express-static')
 const logger = require('express-log')
+const portfinder = require('portfinder')
 
 module.exports = function serve (done) {
+  portfinder.getPort(function (err, port) {
+    if (err) {
+      done(err)
+    } else {
+      const app = express()
 
-  const app = express()
+      app.use(logger())
 
-  app.use(logger())
+      app.use(_static('./'))
 
-  app.use(_static('./'))
+      app.use(function (req, res, next) {
+        res.redirect('/')
+      })
 
-  app.use(function (req, res, next) {
-    res.redirect('/')
+      app.listen(port, function () {
+        console.log('server is running at %s', port)
+      })
+
+      done()
+    }
   })
-
-  app.listen(8088, function () {
-    console.log('server is running at %s', this.address().port)
-  })
-
-  done()
 }
