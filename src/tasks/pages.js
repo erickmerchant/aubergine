516b945bdb8aed8677cad36c03de0214329d25cd
erickmerchant <erickmerchant@gmail.com>
2015-07-27 21:19:33 -0700

    Use atlatl

--- src/tasks/pages.js
+++ src/tasks/pages.js
@@ -1,21 +1,24 @@
 'use strict'
 
-const swig = require('swig')
+const atlatl = require('atlatl')
 const render = require('static-engine-render')
 const read = require('static-engine-read')
 const engine = require('static-engine')
 const cson = require('cson-parser')
 
 module.exports = function pages () {
-  swig.setDefaults({ cache: false })
+  const loader = atlatl('./templates/')
+  const renderer = function (name) {
+    return function (page, done) {
+      loader(name, page, done)
+    }
+  }
 
   return engine([
     read('./content/index.cson'),
     function (pages, done) {
       done(null, [cson.parse(pages[0].content)])
     },
-    render('./index.html', function (page, done) {
-      swig.renderFile('./templates/index.html', page, done)
-    })
+    render('./index.html', renderer('index'))
   ])
 }
