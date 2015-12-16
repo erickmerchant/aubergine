418c4dfffe89802af79cd423bf309c2f09bc3efd
erickmerchant <erickmerchant@gmail.com>
2015-12-15 22:36:40 -0800

    Try webpack

--- tasks/js.js
+++ tasks/js.js
@@ -1,31 +1,22 @@
 'use strict'
 
-const fs = require('fs')
 const chokidar = require('chokidar')
-const browserify = require('browserify')
-const uglify = require('uglify-js')
-const collapse = require('bundle-collapser/plugin')
-const bundler = browserify({debug: false})
+const webpack = require('webpack')
 
 function js () {
   return new Promise(function (resolve, reject) {
-    bundler.add('js/app.js')
-
-    bundler.plugin(collapse)
-
-    bundler.bundle(function (err, js) {
+    webpack({
+      context: __dirname + '/js/',
+      entry: 'app.js',
+      output: {
+        path: './',
+        filename: 'app.js'
+      }
+    }, function (err) {
       if (err) {
         reject(err)
       } else {
-        js = uglify.minify(js.toString(), {fromString: true, mangle: true}).code
-
-        fs.writeFile('./app.js', js, function (err) {
-          if (err) {
-            reject(err)
-          } else {
-            resolve()
-          }
-        })
+        resolve()
       }
     })
   })
