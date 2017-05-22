3477e58e04a0e177212a27ac59646e2b69e14a77
erickmerchant <erickmerchant@gmail.com>
2017-05-21 21:11:37 -0700

    Work on css

--- js/component.js
+++ js/component.js
@@ -29,11 +29,11 @@ module.exports = function ({state, dispatch, next}) {
   <body class="flex column border-box items-center background-dark-gray white min-height-100vh">
     <div class="flex items-center justify-center auto width-3">
       <form class="margin-horizontal-auto width-2 center">
-        <h1 class="desktop-font-size-6vw animated ${state <= 0 ? 'flash' : ''}">${format(state)}</h1>
+        <h1 class="animated ${state <= 0 ? 'flash' : ''}">${format(state)}</h1>
         <div class="flex row mobile-column justify-center wrap auto">
           ${buttons.map((button) => html`
           <div class="margin-2 auto">
-            <button class="button white width-3 padding-2 background-dark-gray white" type="button" onclick=${set(button.time, button.message)}>${button.title}</button>
+            <button class="button white width-3 padding-2 margin-horizontal-1 background-dark-gray white" type="button" onclick=${set(button.time, button.message)}>${button.title}</button>
           </div>`)}
         </div>
         <div class="width-3">
@@ -44,12 +44,14 @@ module.exports = function ({state, dispatch, next}) {
         </div>
       </form>
     </div>
-    <footer class="margin-1 center" role="contentinfo">
+    <footer class="margin-1 center type-small" role="contentinfo">
       <span class="inline-block padding-2">© Erick Merchant, 2017</span>
-      <a class="inline-block white padding-2" href="https://github.com/erickmerchant/chrono">
-        ${icon('github')}
-        View Source
-      </a>
+      <span class="inline-block padding-2">
+        <a class="white" href="https://github.com/erickmerchant/chrono">
+          ${icon('github')}
+          View Source
+        </a>
+      </span>
     </footer>
   </body>
   `
