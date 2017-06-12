c0a79a4b49d8bfb07f4de4a54c88ee81d491cada
erickmerchant <erickmerchant@gmail.com>
2017-06-11 22:13:19 -0700

    Update tests

--- test/component.js
+++ test/component.js
@@ -2,24 +2,16 @@ const component = require('../js/component.js')
 const snapshot = require('assert-snapshot')
 const tape = require('tape')
 
-tape('js/component.js - state 600', function (t) {
-  const str = component({state: 600000}).toString()
+tape('js/component.js - state 300000', function (t) {
+  const str = component({state: 300000}).toString()
 
   snapshot(t, str)
 
   t.end()
 })
 
-tape('js/component.js - state 60', function (t) {
-  const str = component({state: 6000}).toString()
-
-  snapshot(t, str)
-
-  t.end()
-})
-
-tape('js/component.js - state 30', function (t) {
-  const str = component({state: 30000}).toString()
+tape('js/component.js - state 1500000', function (t) {
+  const str = component({state: 1500000}).toString()
 
   snapshot(t, str)
 
