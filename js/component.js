036c75f604be537cc636d75b615474681d2dd54f
erickmerchant <erickmerchant@gmail.com>
2017-10-08 13:25:46 -0700

    Add small shadows

--- js/component.js
+++ js/component.js
@@ -33,21 +33,21 @@ module.exports = function ({state, dispatch, next}) {
   <body class="flex column items-center background-dark-gray white">
     <div class="flex items-center justify-center auto full-width">
       <div class="margin-horizontal-auto full-width max-width align-center">
-        <h1 class="animated ${state.value <= 0 ? 'flash' : ''}">${format(state.value)}</h1>
+        <h1 class="animated ${state.value <= 0 ? 'flash' : ''} text-shadow">${format(state.value)}</h1>
         <div class="flex row mobile-column justify-center wrap auto">
           ${buttons.map((button) => html`
           <div class="mobile-margin-horizontal-4 flex auto">
-            <button class="border-radius border bold full-width padding-2 margin-horizontal-1 margin-vertical-1 background-gray white" type="button" onclick=${() => dispatch(button.time, button.message)}>${button.title}</button>
+            <button class="border-radius border bold full-width padding-2 margin-horizontal-1 margin-vertical-1 background-gray white box-shadow text-shadow" type="button" onclick=${() => dispatch(button.time, button.message)}>${button.title}</button>
           </div>`)}
         </div>
         <div class="full-width">
-          <h2>
+          <h2 class="text-shadow">
             ${icon('clock')} Chrono
           </h2>
         </div>
       </div>
     </div>
-    <footer class="margin-1 align-center font-size-small" role="contentinfo">
+    <footer class="margin-1 align-center font-size-small text-shadow" role="contentinfo">
       <span class="inline-block padding-2">© Erick Merchant, 2017</span>
       <span class="inline-block padding-2">
         <a class="white" href="https://github.com/erickmerchant/chrono">
