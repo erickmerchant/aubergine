8a18d353c10587fa2d57b6619a0fffa95372ba1b
erickmerchant <erickmerchant@gmail.com>
2018-05-25 23:54:39 -0700

    CSS changes

--- js/component.js
+++ js/component.js
@@ -30,22 +30,22 @@ module.exports = function ({state, dispatch, next}) {
     }
   })
 
-  return html`<body class="flex column items-center justify-center background-dark-gray white">
+  return html`<body class="flex column items-center justify-center background-white black">
     <main class="full-width margin-horizontal-auto max-width align-center">
-      <h1 class="animated ${state.value <= 0 ? 'flash' : ''} text-shadow">${format(state.value)}</h1>
+      <h1 class="animated ${state.value <= 0 ? 'flash' : ''}">${format(state.value)}</h1>
       <div class="grid desktop-auto-flow-column mobile-auto-flow-row gap-2 margin-horizontal-2">
         ${buttons.map(button)}
       </div>
       <div class="full-width">
-        <h2 class="text-shadow">
+        <h2 class="">
           ${icon('clock')} Chrono
         </h2>
       </div>
     </main>
-    <footer class="margin-1 align-center font-size-small text-shadow" role="contentinfo">
+    <footer class="margin-1 align-center font-size-6 black" role="contentinfo">
       <span class="inline-block padding-2">${raw('&copy;')} Erick Merchant, 2017</span>
       <span class="inline-block padding-2">
-        <a class="white" href="https://github.com/erickmerchant/chrono">
+        <a class="black" href="https://github.com/erickmerchant/chrono">
           ${icon('github')} View Source
         </a>
       </span>
@@ -54,7 +54,7 @@ module.exports = function ({state, dispatch, next}) {
 
   function button (button) {
     return html`<div>
-      <button class="border-radius border bold full-width padding-2 background-gray white box-shadow text-shadow" type="button" onclick=${() => dispatch(button.time, button.message)}>${button.title}</button>
+      <button class="border-radius border-black bold full-width padding-2 background-white black" type="button" onclick=${() => dispatch(button.time, button.message)}>${button.title}</button>
     </div>`
   }
 }
